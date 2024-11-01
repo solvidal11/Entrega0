97333f5abd1e4ea54954c329aeb8393330c36ca4
Soledad Olivera <adm.olivera95@gmail.com>
2024-11-01 11:37:57 -0300

    "Lista parte Desafiate"

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,10 @@
-
 let carrito = []; // Arreglo para guardar los productos en el carrito
 const DOMitems = document.querySelector('#items');
 const DOMcarrito = document.querySelector('#cart-items');
 const DOMtotal = document.querySelector('#total');
 const DOMbotonVaciar = document.querySelector('#vaciar-carrito');
 
-
- // Dibuja los productos guardados en el carrito.
-
+// Dibuja los productos guardados en el carrito.
 function renderizarCarrito() {
     // Limpiar el HTML del carrito
     DOMcarrito.textContent = '';
@@ -42,7 +39,6 @@ function renderizarCarrito() {
                     <p class="product-moneda">Moneda: ${item.currency}</p>   
                     <p class="product-unitprice">Costo: ${item.price}</p>  
                 </div>  
-    
                 <div id="product-right-info">  
                     Subtotal: <span id="product-subtotal-${item.id}">${item.currency} ${(item.price * item.quantity).toFixed(2)}</span>  
                     <br/>  
@@ -57,7 +53,7 @@ function renderizarCarrito() {
         miBoton.dataset.item = item.id;
         miBoton.addEventListener('click', () => borrarItemCarrito(item.id));
      
-        //Anadir evento de cambio a la cantidad:
+        // Añadir evento de cambio a la cantidad:
         const inputCantidad = miNodo.querySelector('.input-cantidad');
         inputCantidad.addEventListener('input', (event) => {
             const nuevaCantidad = event.target.value;
@@ -74,14 +70,14 @@ function actualizarSubtotal(itemId, price, quantity) {
     const subtotal = document.querySelector(`#product-subtotal-${itemId}`);
     const subtotalAct = (price * quantity).toFixed(2);
 
-// Obtener la moneda del carrito usando el item del localStorage para el subtotal
- const carritoGuardado = JSON.parse(localStorage.getItem('cart')) || [];
- const item = carritoGuardado.find(i => i.id === itemId);
+    // Obtener la moneda del carrito usando el item del localStorage para el subtotal
+    const carritoGuardado = JSON.parse(localStorage.getItem('cart')) || [];
+    const item = carritoGuardado.find(i => i.id === itemId);
 
     subtotal.textContent = `${item.currency} ${subtotalAct}`;
     
-// Actualizar el total
-renderizarTotal();
+    // Actualizar el total
+    renderizarTotal();
 }
 
 // Renderizar el total
@@ -95,25 +91,26 @@ function renderizarTotal() {
         total += item.price * cantidadValor;
     });
 
-    //Asegurar que se muestre la moneda correctamente:
+    // Asegurar que se muestre la moneda correctamente:
     const moneda = carritoGuardado[0]?.currency || '';
     DOMtotal.textContent = `${moneda} ${total.toFixed(2)}`;
 }
 
-//Borrar un producto del carrito.
+// Borrar un producto del carrito.
 function borrarItemCarrito(itemId) {
     const carritoGuardado = JSON.parse(localStorage.getItem('cart')) || []; // Cargar el carrito desde localStorage
     // Filtrar el carrito para eliminar el producto con el id especificado
     const nuevoCarrito = carritoGuardado.filter(item => item.id !== itemId);
     localStorage.setItem('cart', JSON.stringify(nuevoCarrito)); // Guardar el nuevo carrito en localStorage
     renderizarCarrito(); // Volver a renderizar el carrito
+    updateCartBadge(); // Actualizar el badge
 }
 
-//Vaciar carrito.
-
+// Vaciar carrito.
 function vaciarCarrito() {
     localStorage.setItem('cart', JSON.stringify([])); // Vaciar el carrito en localStorage
     renderizarCarrito(); // Volver a renderizar el carrito
+    updateCartBadge(); // Actualizar el badge
 }
 
 // Eventos
